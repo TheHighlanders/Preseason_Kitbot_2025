d0b0e185e214982565c131612bb0c08c994ea9b6
SalmonBug <miningchest@gmail.com>
2025-09-25 16:35:06 -0400

    Drivetrain
    finished drive code

--- Kitbot/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ Kitbot/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -7,23 +7,45 @@ package frc.robot.subsystems;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
  
   private final SparkMax FLMotor 
-    = new SparkMax(1, MotorType.kBrushless);
+    = new SparkMax(1, MotorType.kBrushed);
+
 
   private final SparkMax FRMotor 
-    = new SparkMax(2, MotorType.kBrushless);
+    = new SparkMax(2, MotorType.kBrushed);
 
   private final SparkMax BLMotor 
-    = new SparkMax(3, MotorType.kBrushless);
+    = new SparkMax(3, MotorType.kBrushed);
     
   private final SparkMax BRMotor 
-    = new SparkMax(4, MotorType.kBrushless);
+    = new SparkMax(4, MotorType.kBrushed);
 
-  public Drivetrain() {}
+    private DifferentialDrive drive = new DifferentialDrive(this::left, this::right);
+    
+
+  public Drivetrain() {
+  }
+  public void go(double x, double y) {
+    drive.arcadeDrive(x, y);
+  }
+  
+
+public void left(double left) {
+  FLMotor.set(left);
+  BLMotor.set(left);
+ 
+}
+
+public void right(double right) {
+  FRMotor.set(right);
+  BRMotor.set(right);
+
+} 
 
   @Override
   public void periodic() {
