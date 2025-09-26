ba28faa672beaf04058d6f893e39845bfce973b6
BlockOfWood <ariblock@icloud.com>
2025-09-26 15:39:19 -0400

    code

--- Kitbot/src/main/java/frc/robot/RobotContainer.java
+++ Kitbot/src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,9 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.coralmechinismAri;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -21,18 +23,20 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
+  private final coralmechinismAri mAri= new coralmechinismAri();
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
-
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the trigger bindings
-    configureBindings();
+        private boolean lastAlignSide;
+      
+        /** The container for the robot. Contains subsystems, OI devices, and commands. */
+        public RobotContainer() {
+          // Configure the trigger bindings
+          configureBindings();
+          m_driverController.a().onTrue(Commands.runOnce(() -> this.lastAlignSide = false));
   }
 
-  /**
+  /**`
    * Use this method to define your trigger->command mappings. Triggers can be created via the
    * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
    * predicate, or via the named factories in {@link
--- Kitbot/src/main/java/frc/robot/subsystems/coralmechinismAri.java
+++ Kitbot/src/main/java/frc/robot/subsystems/coralmechinismAri.java
@@ -4,12 +4,25 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class coralmechinismAri extends SubsystemBase {
-  /** Creates a new coralmechinismAri. */
+  /** Creates a new coralmechinismAri. */ 
+  SparkMax motor = new SparkMax(5, MotorType.kBrushed);
   public coralmechinismAri() {}
 
+  public void roll() {
+  motor.set(1);
+}
+public void stopdrop() {
+  motor.set(-1);
+}
+public void grab() {
+  motor.set(0);
+}
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
